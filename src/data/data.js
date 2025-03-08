d116c43e9e96cb3eda9dff1a43acbe54b079b9de
Muhammadali-fr <boburjonr934@gmail.com>
2025-03-08 10:02:11 +0500

    pics

---
+++ src/data/data.js
@@ -0,0 +1,79 @@
+import img1 from "../assets/pics/img1.png";
+import img2 from "../assets/pics/img2.png";
+import img3 from "../assets/pics/img3.png";
+import img4 from "../assets/pics/img4.png";
+import img5 from "../assets/pics/img5.png";
+import img6 from "../assets/pics/img6.png";
+import img7 from "../assets/pics/img7.png";
+import img8 from "../assets/pics/img8.png";
+import img9 from "../assets/pics/img9.png";
+import img10 from "../assets/pics/img10.png";
+
+// pics for home
+export const gallery = [
+  {
+    image: img1,
+    tag: ["development theme", "phone wallpaper"],
+  },
+  {
+    image: img2,
+    tag: ["development theme", "phone wallpaper"],
+  },
+  {
+    image: img3,
+    tag: ["development theme", "phone wallpaper", "desktop wallpaper"],
+  },
+  {
+    image: img4,
+    tag: ["development theme", "desktop wallpaper"],
+  },
+  {
+    image: img5,
+    tag: ["phone wallpaper"],
+  },
+  {
+    image: img6,
+    tag: ["development theme", "phone wallpaper"],
+  },
+  {
+    image: img7,
+    tag: ["development theme", "phone wallpaper"],
+  },
+  {
+    image: img8,
+    tag: ["phone wallpaper", "anime"],
+  },
+  {
+    image: img9,
+    tag: ["phone wallpaper"],
+  },
+  {
+    image: img10,
+    tag: ["anime", "desktop wallpaper"],
+  },
+];
+
+// button categories
+export const filterButtons = [
+  {
+    category: "HARD FILTERING",
+  },
+  {
+    category: "ANIME",
+  },
+  {
+    category: "ANIMALS",
+  },
+  {
+    category: "GAMING",
+  },
+  {
+    category: "DEVELOPMENT THEME",
+  },
+  {
+    category: "DESKTOP WALLPAPER",
+  },
+  {
+    category: "PHONE WALLPAPER",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// filter data 
+import { filterButtons } from '../data/data'
+import { gallery } from "../data/data"
+
+// assets  
+// import FilterImg from "../assets/home/filter.png"
 
 const Home = () => {
+
   return (
-    <div className='w-full'>Home</div>
+    <div className='  w-[95%]  mx-auto py-2'>
+      <ul className='flex gap-2 overflow-x-auto'>
+        {
+          filterButtons.map((filter, index) => (
+            <li key={index} className='py-2 px-4 border-2 border-[#9E9E9E] rounded-lg  hover:border-[#4A249D] cursor-pointer whitespace-nowrap '>
+              <p>{filter.category}</p>
+            </li>
+          ))
+        }
+      </ul>
+
+      {/* pictures  */}
+      <div className="columns-2 sm:columns-3 md:columns-4 lg:columns-5 my-5">
+        {
+          gallery.map((img, index) => (
+            <div key={index}>
+              <img
+                className='w-full mx-auto'
+                src={img.image}
+                alt={img.tag}
+              />
+            </div>
+          ))
+        }
+      </div>
+
+
+    </div>
   )
 }
 
