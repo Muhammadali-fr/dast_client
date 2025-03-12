76dab5556c6086965cb212cbb59b28314ea2f7a1
Muhammadali-fr <boburjonr934@gmail.com>
2025-03-12 19:26:41 +0500

    sdf

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,6 +1,9 @@
 // react router dom 
 import { Link } from "react-router-dom";
 
+// assets 
+import DragPlus from "../assets/addPost/drag.png"
+
 const AddPost = () => {
     return (
         <div className="w-[95%] mx-auto">
@@ -26,10 +29,36 @@ const AddPost = () => {
                 {/* title  */}
                 <p className="text-center text-2xl font-semibold">Create your new post</p>
 
-                <div>
+                <div className="flex justify-between">
                     {/* form  */}
-                    <form>
+                    <form className="max-w-[317px] flex flex-col gap-5">
 
+                        {/* file  */}
+                        <label className="flex flex-col gap-1 cursor-pointer">
+                            <p className="font-bold">Upload your picture</p>
+                            <p className="text-gray-500 text-sm">File types supported: JPG, PNG, GIF, SVG, MP4,</p>
+                            <div className="w-full border border-[#bababa] rounded-lg h-[156px] flex flex-col items-center justify-center">
+                                <img src={DragPlus} alt="drag.png" />
+                                <p className="text-[#bababa]">upload or drag here</p>
+                            </div>
+                            <input className="hidden" type="file" />
+                        </label>
+
+                        {/* title  */}
+                        <label className="w-full flex flex-col gap-1">
+                            <p className="font-bold"> Title of Post</p>
+                            <input className="border border-[#BABABA] p-2 rounded-lg w-full" placeholder="title here" type="text" />
+                        </label>
+
+                        {/* Description  */}
+
+                        <label className="w-full flex flex-col gap-1">
+                            <p className="font-bold">Description</p>
+                            <textarea placeholder="description" className="border border-[#BABABA] p-2 rounded-lg w-full max-h-[84px]"></textarea>
+                        </label>
+
+                        {/* button  */}
+                        <button type="submit" className="w-full cursor-pointer py-2 text-white bg-linear-to-r from-[#4A249D] to-[#7D41FF] rounded-lg hover:opacity-90">Create</button>
                     </form>
 
                     {/* view  */}
